ec0e125aeaea6ba9d66980716de4a2ea70ca4885
ronar01 <105427300+ronar01@users.noreply.github.com>
2023-07-06 09:11:57 -0300

    Apliquei a lógica e melhorei uns aspectos
    Agora possui a função de calcular e de mostrar o resultado na tela.
    >:D

--- lib/CampoTexto.dart
+++ lib/CampoTexto.dart
@@ -9,8 +9,51 @@ class CampoTexto extends StatefulWidget {
 }
 
 class _CampoTextoState extends State<CampoTexto> {
+
   TextEditingController _controllerAlcool = TextEditingController();
   TextEditingController _controllerGasolina = TextEditingController();
+  String _resultado = "";
+  void _calcular() {
+    List<double?> _lista = verificarVirgula();
+    double? _precoAlcool = _lista[0];
+    double? _precoGasolina = _lista[1];
+
+    if(_precoGasolina! > 0 && _precoAlcool! > 0){
+      if((_precoAlcool/_precoGasolina) >= 0.7){
+        setState(() {
+          _resultado = "melhor abastecer com gasolina";
+        });
+      }else{
+        setState(() {
+          _resultado = "Melhor abastecer com alcool";
+        });
+      }
+    }else{
+      setState(() {
+        _resultado = "Valor nulo ou negativo!";
+      });
+    }
+
+  }
+    List<double?> verificarVirgula(){
+      double? _precoAlcool;
+      double? _precoGasolina;
+      if(_controllerAlcool.text.contains(",")){
+        String formatado = _controllerAlcool.text.trim().replaceAll(",", ".");
+        _precoAlcool = double.tryParse(formatado);
+      }else{
+        _precoAlcool = double.tryParse(_controllerAlcool.text.trim());
+      }
+      if(_controllerGasolina.text.contains(",")){
+        String formatado1 = _controllerGasolina.text.trim().replaceAll(",", ".");
+        _precoGasolina = double.tryParse(formatado1);
+      }else{
+        _precoGasolina = double.tryParse(_controllerGasolina.text.trim());
+      }
+      return [_precoAlcool, _precoGasolina];
+    }
+
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -25,8 +68,7 @@ class _CampoTextoState extends State<CampoTexto> {
               ),
             ),
           ),
-          body: Container(
-            child: SingleChildScrollView(
+          body: SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
@@ -64,7 +106,7 @@ class _CampoTextoState extends State<CampoTexto> {
                   Padding(
                     padding: EdgeInsets.only(left: 32, right: 32),
                     child: ElevatedButton(
-                      onPressed: (){},
+                      onPressed: _calcular,
                       style: ButtonStyle(
                         backgroundColor: MaterialStatePropertyAll(Colors.deepPurpleAccent),
                       ),
@@ -75,12 +117,20 @@ class _CampoTextoState extends State<CampoTexto> {
                         ),
                       ),
                     ) ,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 32, right: 32, top: 20),
+                    child: Text(
+                      _resultado,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
                   )
                 ],
               ),
             ),
           )
-      ),
-    );
+      );
   }
 }
